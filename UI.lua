1462a389865f7a4e5db4bb5a806a3f05a1c595e7
rebenkoy <yarebenko@gmail.com>
2023-04-29 10:12:29 +0000

    Git branch puller set up

--- UI.lua
+++ UI.lua
@@ -34,6 +34,28 @@ local menu = workspace:addChild(GUI.menu(1, 1, workspace.width, cs.menu.bg, cs.m
 menu:addItem("AE2 StockKeeper", 0x0)
 
 menu:addItem("Reboot").onTouch = function() os.execute("reboot") end
+menu:addItem("Pull").onTouch = function() filesystem.copy("current_branch", "branch") end
+menu:addItem("Set branch").onTouch = function()
+  local filename = "current_branch"
+  local file, err = io.open(filename, "r")
+  local current_branch = ""
+  if not (err ~= nil) then
+    current_branch = file:read()
+    file:close()
+  end
+
+	local container = GUI.addBackgroundContainer(workspace, true, true, "Change current git branch")
+  local input = GUI.input(1, 1, 30, 3, 0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, current_branch, "Enter branch name")
+  input.onInputFinished = function()
+    local file, err = io.open(filename, "r")
+    if not (err == nil) then
+      GUI.alert(string.format("Can not open file [%s] for write", filename))
+    end
+    file:write(input.text)
+    file:close()
+  end
+	container.layout:addChild(input)
+end
 
 
 local cs_menu = menu:addContextMenuItem("Preferences")
@@ -49,7 +71,6 @@ cs_menu:addItem("Set default sizes")
 cs_menu:addItem("Copy sizes")
 cs_menu:addItem("Alter sizes")
 cs_menu:addItem("Delete sizes")
-
 
 local body = require("body")(workspace, sz.menu, sz, cs)
 
